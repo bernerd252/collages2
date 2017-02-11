7cbd34eec3c60d3ee0c8dc837c366b03d0b073df
bernerd252 <edouardbatlle@yahoo.com>
2017-02-11 11:57:22 -0800

    mouth added

--- 2.11.17/js/index.js
+++ 2.11.17/js/index.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('#putin').click(function(){
-		$("#putin").attr('src', "https://raw.githubusercontent.com/bernerd252/collages2/master/2.11.17/images/putinHeadOff.png"),
+		$("#putin").attr('src', "https://raw.githubusercontent.com/bernerd252/collages2/master/2.11.17/images/putinMouthOpen.png"),
 		$("#putinImage").append("<div id ='trumpImage'><img id ='trump' src='http://i.giphy.com/znrnkBB5sKfhS.gif'></div>")
-	})
-})
+		$("#putinImage").append("<div id ='putinMouthImage'><img id ='putinMouth' src='https://raw.githubusercontent.com/bernerd252/collages2/master/2.11.17/images/putinMouth.png'></div>")
+});
+});
