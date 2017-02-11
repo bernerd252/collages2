3abbfd0c99702dd444544b5fbb8a9b7fbc784329
bernerd252 <edouardbatlle@yahoo.com>
2017-02-11 13:14:13 -0800

    mouth moves

--- 2.11.17/js/index.js
+++ 2.11.17/js/index.js
@@ -3,5 +3,6 @@ $(function() {
 		$("#putin").attr('src', "https://raw.githubusercontent.com/bernerd252/collages2/master/2.11.17/images/putinMouthOpen.png"),
 		$("#putinImage").append("<div id ='trumpImage'><img id ='trump' src='http://i.giphy.com/znrnkBB5sKfhS.gif'></div>")
 		$("#putinImage").append("<div id ='putinMouthImage'><img id ='putinMouth' src='https://raw.githubusercontent.com/bernerd252/collages2/master/2.11.17/images/putinMouth.png'></div>")
+		
 });
 });
